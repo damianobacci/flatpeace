a774975e707315219c7ed7018ec6ed5992422ca9
Damiano Bacci <damiano.bacci@gmail.com>
2024-07-10 08:55:11 +0200

    Setup of a basic server with dev environment

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("server is working");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import morgan from "morgan";
+
+const port = process.env.PORT || 5100;
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.json({ message: "Data received!", data: req.body });
+});
+
+app.listen(port, () => {
+  console.log("Server is running...");
+});
