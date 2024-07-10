96e6fd20376bb049dfe67c95f1a14879a66d6503
Damiano Bacci <damiano.bacci@gmail.com>
2024-07-10 11:14:17 +0200

    Added mongoose connection and express-async-errors

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+// DONTENV
 import * as dotenv from "dotenv";
 dotenv.config();
 
+// IMPORTS
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
 
+// ROUTERS
 import flatshareRouter from "./routers/flatsharesRouter.js";
+
+//-----------------------
 
 const port = process.env.PORT || 5100;
 
@@ -26,6 +32,12 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Something went wrong..." });
 });
 
-app.listen(port, () => {
-  console.log("Server is running...");
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`Server is running on PORT ${port}...`);
+  });
+} catch {
+  console.log(error);
+  process.exit(1);
+}
