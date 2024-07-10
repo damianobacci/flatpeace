4925eca114edcc68a41f8c2c3ed5e4257732684a
Damiano Bacci <damiano.bacci@gmail.com>
2024-07-10 10:33:42 +0200

    Basic CRUD operations on a test array

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ dotenv.config();
 
 import express from "express";
 import morgan from "morgan";
+import { nanoid } from "nanoid";
+
+let flatshares = [
+  {
+    id: nanoid(),
+    name: "test",
+    members: [nanoid(), nanoid()],
+  },
+  {
+    id: nanoid(),
+    name: "test",
+    members: [nanoid(), nanoid()],
+  },
+];
 
 const port = process.env.PORT || 5100;
 
@@ -14,12 +28,62 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/api/v1/flatshares", (req, res) => {
+  res.status(200).json({ flatshares });
 });
 
-app.post("/", (req, res) => {
-  res.json({ message: "Data received!", data: req.body });
+app.get("/api/v1/flatshares/:id", (req, res) => {
+  const { id } = req.params;
+  const flatshare = flatshares.find((flatshare) => flatshare.id === id);
+  if (!flatshare) {
+    return res.status(404).json({ message: `No job with id ${id} found.` });
+  }
+  res.status(200).json({ flatshare });
+});
+
+app.patch("/api/v1/flatshares/:id", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ message: "Please provide a name for your flatshare" });
+  }
+  const { id } = req.params;
+  const flatshare = flatshares.find((flatshare) => flatshare.id === id);
+  if (!flatshare) {
+    return res.status(404).json({ message: `No job with id ${id} found.` });
+  }
+  flatshare.name = name;
+  res.status(200).json({ message: "Flatshare modified", flatshare });
+});
+
+app.delete("/api/v1/flatshares/:id", (req, res) => {
+  const { id } = req.params;
+  const flatshare = flatshares.find((flatshare) => flatshare.id === id);
+  if (!flatshare) {
+    return res.status(404).json({ message: `No job with id ${id} found.` });
+  }
+  const newFlatshares = flatshares.filter((flatshare) => flatshare.id !== id);
+  flatshares = newFlatshares;
+  res.status(200).json({ message: "Flatshare deleted" });
+});
+
+app.post("/api/v1/flatshares", (req, res) => {
+  const name = req.body.name;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ message: "Please provide a name for your flatshare" });
+  }
+  const id = nanoid();
+  const members = [nanoid(), nanoid()];
+  const flatshare = {
+    id,
+    name,
+    members,
+  };
+  flatshares.push(flatshare);
+  res.status(200).json({ flatshare });
 });
 
 app.listen(port, () => {
