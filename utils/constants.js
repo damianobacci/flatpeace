0570bb9e0f40d3d18c3e0c99ae252fcdd8b11dd1
Damiano Bacci <damiano.bacci@gmail.com>
2024-07-11 10:34:41 +0200

    Constants utils file

--- utils/constants.js
+++ utils/constants.js
@@ -3,3 +3,10 @@ export const STATUS = {
   INTERVIEW: "interview",
   DECLINED: "declined",
 };
+
+export const SORT_BY = {
+  NEWEST_FIRST: "newest",
+  OLDEST_FIRST: "oldest",
+  ASCENDING: "a-z",
+  DESCENDING: "z-a",
+};
